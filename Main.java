f31619fd6aa903f598b9e69733b8d3e24d2e6ab9
gxiaozi <1906716331@qq.com>
2024-07-09 13:49:38 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("你好啊112233");
+    }
+    
+}
+class Solution {
+    public int specialPerm(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            
+        }
+        return 0;
+    }
+}
